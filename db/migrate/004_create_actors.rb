cb5253143f889d0e0e1c46516a72ad3e61a76684
Jason Vance <jbvance@gmail.com>
2017-12-06 20:31:38 +0000

    automatically backed up by learn

--- db/migrate/004_create_actors.rb
+++ db/migrate/004_create_actors.rb
@@ -1,9 +1,9 @@
-class CreateCharacters < ActiveRecord::Migration
+class CreateActors < ActiveRecord::Migration
   #define a change method in which to do the migration
   def change
-    create_table :characters do |t| #we get a block variable here for the table
-      t.string name
-      t.integer show_id
+    create_table :actors do |t| #we get a block variable here for the table
+      t.string first_name
+      t.string last_name
     end
   end
 end
